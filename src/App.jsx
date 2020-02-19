8372ed7485030bb0db97e9ef65889e089793129b
WStrellis <wstrellis@gmail.com>
2020-02-19 11:59:19 -0500

    configured webpack

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {app, zerb, nerk, grayImage} from "./styles/App.module.scss"
+
+const App = props => {
+    return (
+        <div className="blah">
+            <div className={app}>
+                <p>I&apos;m a SCSS module</p>
+                <p className={`${zerb} ${nerk}`}>{props.title}</p>
+                <div className={grayImage}></div>
+            </div>
+        </div>
+    )
+}
+
+export default App
