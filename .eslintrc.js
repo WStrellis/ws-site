c01c82cf8e7fb84f8bebcb199fcbe2d01e88f79a
WStrellis <wstrellis@gmail.com>
2020-02-22 14:14:00 -0500

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        jest: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: ["react", "jsx-a11y"],
+    settings: {
+        react: {
+            pragma: "React",
+            version: "detect",
+        },
+    },
+    rules: {
+        "no-unused-vars": 0,
+    },
+}
