c16c1acc0fa763053eb440bd3bd53a10fdea50f8
WStrellis <wstrellis@gmail.com>
2020-02-28 17:33:24 -0500

    added apod images

--- src/mui-theme.js
+++ src/mui-theme.js
@@ -13,10 +13,14 @@ import orange from "@material-ui/core/colors/orange"
 // const PRIMARY_LIGHT = "hsl(104deg, 41%, 80%)"
 // const PRIMARY = "hsl(104deg, 41%, 65%)"
 // const PRIMARY_DARK = "hsl(104deg, 41%, 50%)"
+//light yellow
+// const PRIMARY_LIGHT = "hsl(49deg, 59%, 80%)"
+// const PRIMARY = "hsl(49deg, 59%, 65%)"
+// const PRIMARY_DARK = "hsl(49deg, 59%, 50%)"
 
-const PRIMARY_LIGHT = "hsl(49deg, 59%, 80%)"
-const PRIMARY = "hsl(49deg, 59%, 65%)"
-const PRIMARY_DARK = "hsl(49deg, 59%, 50%)"
+const PRIMARY_LIGHT = "hsl(219deg, 10%, 40%)"
+const PRIMARY = "hsl(219deg, 10%, 25%)"
+const PRIMARY_DARK = "hsl(219deg, 10%, 10%)"
 
 export default createMuiTheme({
     palette: {
@@ -40,8 +44,8 @@ export default createMuiTheme({
     overrides: {
         MuiButton: {
             contained: {
-                boxShadow: `-2px -2px 5px ${PRIMARY_DARK}, 
-             2px 2px 5px ${PRIMARY_LIGHT}`,
+                boxShadow: `inset 2px 2px 5px ${PRIMARY_DARK}, 
+             inset -2px -2px 5px ${PRIMARY_LIGHT}`,
                 "&:hover": {
                     boxShadow: `0 0 #00000000, 
              0 0 #00000000`,
